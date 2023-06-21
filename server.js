f884a846d77211284ba91afcd0a4cbee0e9041b9
Aple <aple@Aples-MacBook-Air.local>
2023-06-21 20:44:07 +0700

    update server

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const http = require('http').createServer(app);
-const io = require('socket.io')(http);
+const socket = io('https://simple-chat-mu.vercel.app/');
 
 app.use(express.static(__dirname + '/public'));
 
