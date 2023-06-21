78fd76e1efd8e4906b843704b7ad20512586247f
unknown <markus.sarsanto@gmail.com>
2023-06-21 22:31:04 +0700

    update server 1.2

--- server.js
+++ server.js
@@ -1,8 +1,31 @@
+// const express = require('express');
+// const app = express();
+// const http = require('http').createServer(app);
+// const socket = io('https://simple-chat-mu.vercel.app/');
+
+// app.use(express.static(__dirname + '/public'));
+
+// io.on('connection', socket => {
+//   socket.on('message', data => {
+//     const { username, message } = data;
+//     const chatMessage = { sender: username, message: message };
+//     io.emit('message', chatMessage);
+//   });
+// });
+
+// const port = process.env.PORT || 3000;
+// http.listen(port, () => {
+//   console.log(`Server running on port ${port}`);
+// });
+
+
 const express = require('express');
+const cors = require('cors'); // Import the cors module
 const app = express();
 const http = require('http').createServer(app);
-const socket = io('https://simple-chat-mu.vercel.app/');
+const io = require('socket.io')(http); // Fix the socket initialization
 
+app.use(cors()); // Enable CORS for all routes
 app.use(express.static(__dirname + '/public'));
 
 io.on('connection', socket => {
